067316dccbe1d139d8ef10b494cc1e32fcceceaa
FrankHsiao <lordarcher12345@gmail.com>
2022-06-19 16:22:14 +0800

    Build Environment

---
+++ cloud-provider-payment8001/src/main/java/com/frank/springcloud/PaymentMainApplication.java
@@ -0,0 +1,13 @@
+package com.frank.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentMainApplication.class, args);
+	}
+
+}
