74f17b1278215e93e2e56add4e8b48f3a61e5c02
FrankHsiao <lordarcher12345@gmail.com>
2022-06-26 14:26:34 +0800

    add eureka client, log properties

--- cloud-customer-order80/src/main/java/com/frank/springcloud/order80/OrderMainApplication.java
+++ cloud-customer-order80/src/main/java/com/frank/springcloud/order80/OrderMainApplication.java
@@ -2,8 +2,10 @@ package com.frank.springcloud.order80;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMainApplication {
 
 	public static void main(String[] args) {
